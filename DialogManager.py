b2b406cf3a58608c0ea21dd5e5a107cc77ed4606
Fadi Botros <fadibotros@Fadis-MacBook-Pro.local>
2017-03-23 22:33:12 -0400

    implemented symptoms

--- DialogManager.py
+++ DialogManager.py
@@ -9,6 +9,7 @@ class DialogManager():
 
 	def __init__(self):
 		self.retval = Retrieval()
+		self.currentIllness = None
 		i =1
 
 	def build_speechlet_response(self,title, output, reprompt_text, should_end_session):
@@ -41,39 +42,55 @@ class DialogManager():
 	# --------------- Functions that control the skill's behavior ------------------
 	def say_hello(self):
 		session_attributes = {}
-		card_title = "Hi, I'm doctor Mellon. You can tell me your symptoms and I will try to diagnose you."
-		speech_output = "Hi, I'm doctor Mellon. You can tell me your symptoms and I will try to diagnose you."
+		card_title = "Hi, I'm doctor Mellon. How can I assisst you?"
+		speech_output = "Hi, I'm doctor Mellon. How can I assisst you?"
 		reprompt_text = "What can I help you with?"
 		should_end_session = False
 		return self.build_response(session_attributes, self.build_speechlet_response(
 			card_title, speech_output, reprompt_text, should_end_session))
 
 	def stop(self):
-		card_title = "Have a nice day!"
-		speech_output = "Have a nice day!"
+		output = "Have a nice day!"
 		should_end_session = True
 		return build_response({}, self.build_speechlet_response(
-			card_title, speech_output, None, should_end_session))
+			output, output, None, should_end_session))
 
 	def help(self):
-		card_title = "What do you want to talk about?"
-		speech_output = "Let's have a conversation. What do you want to talk about?"
-		reprompt_text = "What do you want to talk about?"
+		output = "How can I assist you ?"
 		should_end_session = False
 		return build_response({}, self.build_speechlet_response(
-			card_title, speech_output, reprompt_text, should_end_session))
+			output, output, output, should_end_session))
 
 	def handleDefinitionIntent(self,intentObj):
 		illnessName = intentObj["slots"]["Illnesses"]["value"]
 		illness = self.retval.getIllnessByName(illnessName)
+		self.currentIllness = illness
 
-		card_title = "About: " + illness["title"]
-		speech_output = illness["definition"][0]
-		reprompt_text = illness["definition"][0]
+		output = illness["definition"][0]
 		should_end_session = False
 
 		return self.build_response({}, self.build_speechlet_response(
-			card_title, speech_output, reprompt_text, should_end_session))
+			output, output, output, should_end_session))
+
+	def handleSymptomsIntent(self,intentObj):
+		should_end_session = False
+		output = None
+
+		# check if user specified an illness
+		if "value" in intentObj["slots"]["Illnesses"]:
+			illnessName = intentObj["slots"]["Illnesses"]["value"]
+			illness = self.retval.getIllnessByName(illnessName)
+
+			output = " ".join(illness["symtopn"][:2])
+			should_end_session = False
+		elif self.currentIllness != None:
+			output = " ".join(self.currentIllness["symtopn"][:2])
+		else:
+			output = "Sorry I didn't quite understand that..."
+
+		return self.build_response({}, self.build_speechlet_response(
+				output, output, output, should_end_session))
+
 
 
 	def on_intent(self,intent_request, session):
